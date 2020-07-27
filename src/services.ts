5a11c5cce3881d10dc3eff517c8d48c6220f6815
abiola ojo <ojoabiolavictor@gmail.com>
2020-07-27 21:31:00 +0100

    initial commit

--- src/services.ts
+++ src/services.ts
@@ -4,7 +4,7 @@ import { location } from "./types";
 
 export const getCityLonLat = async (city: string) => {
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&fields=geometry&key=AIzaSyAPgfnbQGDpJj7ohh3ahkdEnFhglVlr_RY`;
+  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&fields=geometry&key=${process.env.REACT_APP_GOOGLE}`;
   const { status, data } = await axios.post(proxyurl + url);
   if (status === 200 && data.candidates[0]) {
     return data.candidates[0].geometry.location;
@@ -15,7 +15,7 @@ export const getCityLonLat = async (city: string) => {
 
 export const getWeatherCondition = async (location: location) => {
   const { status, data } = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lng}&units=metric&appid=933ee340a92358071bc365e3b9057a89&exclude=hourly,minutely`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lng}&units=metric&appid=${process.env.REACT_APP_WEATHER}&exclude=hourly,minutely`
   );
   if (status === 200) {
     return data;
